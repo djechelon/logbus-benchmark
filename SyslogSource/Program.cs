de2bd0eb597da0dd454ff4d1f60a4b9f6eee6321
/usr/local/ΕΨΗΕΛΩΝ <djechelon@users.noreply.github.com>
2010-07-28 23:11:54 +0000

    The most stupid error you can do about for cycles

--- SyslogSource/Program.cs
+++ SyslogSource/Program.cs
@@ -66,7 +66,7 @@ namespace SyslogSource
 
             System.Threading.Thread.Sleep(3000);
 
-            for (int i = 0; i < messages - 1; i++)
+            for (int i = 0; i < messages; i++)
             {
                 SyslogMessage message = new SyslogMessage()
                 {
