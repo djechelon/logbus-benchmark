eb68969021c7cb5db31931999f30ee103ec47314
djechelon <djechelon@0bd441a8-1b1f-446d-9fde-d357b021e4f3>
2010-09-27 19:44:17 +0000

    Added heartbeat simulator and monitor

---
+++ trunk/logbus-benchmark/HeartbeatMonitor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using It.Unina.Dis.Logbus.Clients;
+using It.Unina.Dis.Logbus.Filters;
+using System;
+using It.Unina.Dis.Logbus;
+namespace HeartbeatMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program monitors only heartbeats coming from any entity in the system.");
+            Console.WriteLine("Press CTRL+C when you are finished.");
+
+            PropertyFilter idFilt = new PropertyFilter()
+                                         {
+                                             comparison = ComparisonOperator.eq,
+                                             propertyName = Property.MessageID,
+                                             value = "HEARTBEAT"
+                                         };
+            SeverityFilter debugFilter = new SeverityFilter() { comparison = ComparisonOperator.eq, severity = Severity.Debug };
+            
+            using (ILogClient client = ClientHelper.CreateDefaultClient(idFilt & debugFilter))
+            {
+                client.MessageReceived += new System.EventHandler<It.Unina.Dis.Logbus.SyslogMessageEventArgs>(client_MessageReceived);
+                client.Start();
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+
+        static void client_MessageReceived(object sender, It.Unina.Dis.Logbus.SyslogMessageEventArgs e)
+        {
+            SyslogAttributes attrs = e.Message.GetAdvancedAttributes();
+            Console.WriteLine("Got heartbeat from ({0}|{1}|{2})", e.Message.Host,
+                              e.Message.ProcessID ?? e.Message.ApplicationName, attrs.LogName);
+        }
+    }
+}
---
+++ trunk/logbus-benchmark/HeartbeatSource/Program.cs
@@ -0,0 +1,42 @@
+﻿using It.Unina.Dis.Logbus.Loggers;
+using System.Threading;
+using System;
+namespace HeartbeatSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint entities = 1;
+            Console.WriteLine("This program periodically sends heartbeats to Logbus.");
+            if (args.Length > 0)
+            {
+                if (uint.TryParse(args[0], out entities)) Console.WriteLine("Using {0} entities for this process", entities);
+                else
+                {
+                    Console.WriteLine("Usage: HeartbeatSource [ENTITIES]");
+                    Console.WriteLine("\tENTITIES:\tNumber of entities that this program will simulate");
+                    Environment.Exit(1);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Activating {0} entities. This may take a while as the program waits a little time between activations...");
+            ILog[] logs = new ILog[entities];
+
+            Random rand = new Random();
+            for (int i = 0; i < entities; i++)
+            {
+                logs[i] = LoggerHelper.GetLogger("HbEntity-" + i.ToString());
+                Thread.Sleep(rand.Next(3000));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Program running. No status will be reported here.");
+            Console.WriteLine("Press CTRL+C when you want to stop.");
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+
+    }
+}
